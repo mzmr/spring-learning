1918c7221e0f8b76cda8bc12061ab63421992c67
Maciej Znamirowski <maciek@znamirowski.pl>
2019-01-05 13:12:40 +0100

    Updating data in DB

---
+++ hibernate-tutorial/src/pl/znamirowski/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,46 @@
+package pl.znamirowski.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import pl.znamirowski.hibernate.demo.entity.Student;
+
+public class UpdateStudentDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+								 .configure()
+								 .addAnnotatedClass(Student.class)
+								 .buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			
+			int studentId = 1;
+			System.out.println("\nGetting student with id: " + studentId);
+			Student myStudent = session.get(Student.class, studentId);
+			
+			System.out.println("Updating student...");
+			myStudent.setFirstName("Scooby");
+			
+			session.getTransaction().commit();
+			
+			
+			
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			System.out.println("Update email for all students");
+			session.createQuery("update Student set email='foo@gmail.com'").executeUpdate();
+			
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		} finally {
+			factory.close();
+		}
+	}
+
+}
