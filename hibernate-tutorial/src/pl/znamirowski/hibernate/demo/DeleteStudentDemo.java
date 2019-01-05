a315e91c45cd4f9576bcc4181cecf8175a0f4f90
Maciej Znamirowski <maciek@znamirowski.pl>
2019-01-05 13:30:50 +0100

    Deleting data from DB

--- hibernate-tutorial/src/pl/znamirowski/hibernate/demo/DeleteStudentDemo.java
+++ hibernate-tutorial/src/pl/znamirowski/hibernate/demo/DeleteStudentDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 import pl.znamirowski.hibernate.demo.entity.Student;
 
-public class UpdateStudentDemo {
+public class DeleteStudentDemo {
 
 	public static void main(String[] args) {
 		SessionFactory factory = new Configuration()
@@ -23,16 +23,20 @@ public class UpdateStudentDemo {
 			System.out.println("\nGetting student with id: " + studentId);
 			Student myStudent = session.get(Student.class, studentId);
 			
-			System.out.println("Deleting student: " + myStudent);
-			session.delete(myStudent);
-			
-			
-			
-			System.out.println("Deleting student id=2");
-			session.createQuery("delete from Student where id=2").executeUpdate();
+			System.out.println("Updating student...");
+			myStudent.setFirstName("Scooby");
 			
 			session.getTransaction().commit();
 			
+			
+			
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			System.out.println("Update email for all students");
+			session.createQuery("update Student set email='foo@gmail.com'").executeUpdate();
+			
+			session.getTransaction().commit();
 			System.out.println("Done!");
 		} finally {
 			factory.close();
