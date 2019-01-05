c9b8497d6bfefb083f672659fe2aa7313adfdb3e
Maciej Znamirowski <maciek@znamirowski.pl>
2019-01-05 18:05:35 +0100

    Added hibernate-one-to-one-bi-dir project

---
+++ hibernate-one-to-one-bi-dir/src/pl/znamirowski/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,41 @@
+package pl.znamirowski.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import pl.znamirowski.hibernate.demo.entity.Instructor;
+import pl.znamirowski.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+								 .configure()
+								 .addAnnotatedClass(Instructor.class)
+								 .addAnnotatedClass(InstructorDetail.class)
+								 .buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			
+			int theId = 2;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			System.out.println("tempInstructionDetail: " + tempInstructorDetail);
+			
+			System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor());
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
