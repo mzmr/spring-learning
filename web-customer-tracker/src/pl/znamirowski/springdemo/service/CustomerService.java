0a84ab7886113bd265a495f0ce975b1bfad670db
Maciej Znamirowski <maciek@znamirowski.pl>
2019-01-07 18:41:21 +0100

    Inserted new layer (service layer) between controller and DAO

---
+++ web-customer-tracker/src/pl/znamirowski/springdemo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package pl.znamirowski.springdemo.service;
+
+import java.util.List;
+
+import pl.znamirowski.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
---
+++ web-customer-tracker/src/pl/znamirowski/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,25 @@
+package pl.znamirowski.springdemo.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pl.znamirowski.springdemo.dao.CustomerDAO;
+import pl.znamirowski.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+}
