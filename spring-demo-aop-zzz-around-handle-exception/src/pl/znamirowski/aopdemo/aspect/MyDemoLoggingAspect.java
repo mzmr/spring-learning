f3688ad8f07c8470cecf3eb264e544d44a9bf289
Maciej Znamirowski <maciek@znamirowski.pl>
2019-01-09 22:33:27 +0100

    Added spring-demo-aop-zzz-around-handle-exception project

---
+++ spring-demo-aop-zzz-around-handle-exception/src/pl/znamirowski/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,108 @@
+package pl.znamirowski.aopdemo.aspect;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import pl.znamirowski.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	private Logger myLogger = Logger.getLogger(MyDemoLoggingAspect.class.getName());
+	
+	@Around("execution(* pl.znamirowski.aopdemo.service.*.getFortune(..))")
+	public Object aroundGetFortune(ProceedingJoinPoint theProceedingJoinPoint) throws Throwable {
+		String method = theProceedingJoinPoint.getSignature().toShortString();
+		myLogger.info("\n====>>> Executing @Around on method: " + method);
+		
+		long begin = System.currentTimeMillis();
+		Object result = null;
+		
+		try {
+			result = theProceedingJoinPoint.proceed();
+		} catch (Exception e) {
+			myLogger.warning(e.getMessage());
+			result = "Major accident! But no worries, your private AOP helicopter is on the way!";
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		long duration = end - begin;
+		myLogger.info("\n====>>> Duration: " + duration / 1000.0 + " seconds");
+		return result;
+	}
+	
+	@After("execution(* pl.znamirowski.aopdemo.dao.AccountDAO.findAccounts(..))")
+	public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
+		String method = theJoinPoint.getSignature().toShortString();
+		myLogger.info("\n====>>> Executing @After (finally) on method: " + method);
+	}
+	
+	@AfterThrowing(
+			pointcut="execution(* pl.znamirowski.aopdemo.dao.AccountDAO.findAccounts(..))",
+			throwing="theExc")
+	public void afterThrowingFindAccountsAdvice(JoinPoint theJoinPoint, Throwable theExc) {
+		String method = theJoinPoint.getSignature().toShortString();
+		myLogger.info("\n====>>> Executing @AfterThrowing on method: " + method);
+		
+		myLogger.info("\n====>>> The exception is: " + theExc);
+	}
+	
+	@AfterReturning(
+			pointcut="execution(* pl.znamirowski.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
+		String method = theJoinPoint.getSignature().toShortString();
+		myLogger.info("\n====>>> Executing @AfterReturning on method: " + method);
+		
+		myLogger.info("\n====>>> Result is: " + result);
+		
+		convertAccountNamesToUpperCase(result);
+		
+		myLogger.info("\n====>>> Result is: " + result);
+	}
+	
+
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+		for (Account tempAccount : result) {
+			String theUpperName = tempAccount.getName().toUpperCase();
+			tempAccount.setName(theUpperName);
+		}
+	}
+
+
+	@Before("pl.znamirowski.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+		myLogger.info("\n====>>> Executing @Before advice on addAccount()");
+		
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		myLogger.info("Method: " + methodSig);
+		
+		Object[] args = theJoinPoint.getArgs();
+		
+		for (Object tempArg : args) {
+			myLogger.info(tempArg.toString());
+			
+			if (tempArg instanceof Account) {
+				Account theAccount = (Account) tempArg;
+				myLogger.info("account name: " + theAccount.getName());
+				myLogger.info("account level: " + theAccount.getLevel());
+			}
+		}
+	}
+
+}
---
+++ spring-demo-aop-zzz-around-handle-exception/src/pl/znamirowski/aopdemo/service/TrafficFortuneService.java
@@ -0,0 +1,26 @@
+package pl.znamirowski.aopdemo.service;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrafficFortuneService {
+
+	public String getFortune() {
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		return "Expect heavy traffic this morning";
+	}
+
+	public String getFortune(boolean tripWire) {
+		if (tripWire) {
+			throw new RuntimeException("Major accident! Highway is closed!");
+		}
+		return getFortune();
+	}
+}
