1763c748d0f01edf055084950e860b786c859e6b
Maciej Znamirowski <maciek@znamirowski.pl>
2019-01-22 21:30:42 +0100

    Updated web-customer-tracker-aop project and added two example empty maven projects

---
+++ web-customer-tracker-aop/src/pl/znamirowski/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,50 @@
+package pl.znamirowski.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	@Pointcut("execution(* pl.znamirowski.springdemo.controller.*.*(..))")
+	private void forControllerPackage() { }
+	
+	@Pointcut("execution(* pl.znamirowski.springdemo.service.*.*(..))")
+	private void forServicePackage() { }
+	
+	@Pointcut("execution(* pl.znamirowski.springdemo.dao.*.*(..))")
+	private void forDaoPackage() { }
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() { }
+	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("====>>> in @Before: calling method: " + theMethod);
+		
+		Object[] args = theJoinPoint.getArgs();
+		
+		for (Object tempArg : args) {
+			myLogger.info("====>>> argument: " + tempArg);
+		}
+	}
+	
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult")
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("====>>> in @AfterReturning: from method: " + theMethod);
+		myLogger.info("====>>> result: " + theResult);
+	}
+}
